c34996e98532f290ea263a84c225ccace6ceb7b7
Dulcelene Machado <dudutola@gmail.com>
2025-02-25 12:39:58 +0000

    Installed faker gem Added 10 new articles in seeds using faker

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,29 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+puts "Deleting all existing articles..."
+
+Article.delete_all
+
+puts "All articles deleted."
+
+puts "Creating 10 fake articles..."
+
+10.times do
+  Article.create(
+    title: Faker::Lorem.sentence(word_count: 3),
+    content: [
+      Faker::Lorem.sentence(word_count: 5),
+      Faker::Lorem.paragraph(sentence_count: 5),
+      Faker::Lorem.sentence(word_count: 5),
+      Faker::Lorem.paragraph(sentence_count: 5),
+      Faker::Lorem.sentence(word_count: 5),
+      Faker::Lorem.paragraph(sentence_count: 5)
+    ].join("\n\n"),
+    created_at: Faker::Time.backward(days: 365),
+    updated_at: Faker::Time.backward(days: 30)
+  )
+end
+
+puts "10 fake articles created!"
